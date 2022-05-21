433c9a7df8e8480115f09d6105da4c1019c22c1e
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2022-05-21 13:44:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+function folders() {
+  document.location = "https://github.com/MelanyNadine/free-articles";
+  let foldersAndFiles = document.getElementsByClassName('js-navigation-open Link--primary');
+  console.log(foldersAndFiles);
+}
+
 function x(variable_name) {
   return document.getElementById(variable_name);
 }
