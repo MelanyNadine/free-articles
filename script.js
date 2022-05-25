a4a8aa0537da20253dc38a3627e93a8eb6ea7f59
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2022-05-25 17:48:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+const folders = {
+ 	"Physics": "https://u.pcloud.link/publink/show?code=kZpwpzVZW7GjLH2RTnXTxf5oOrsqUJ70416V",
+	"Mathematics": "https://u.pcloud.link/publink/show?code=kZ3lpzVZWuRbUhACQkQkqrRrgqA2dyab5Gg7",
+	"Information Theory": "https://u.pcloud.link/publink/show?code=kZ3lpzVZWuRbUhACQkQkqrRrgqA2dyab5Gg7",
+	"Information Security": "https://u.pcloud.link/publink/show?code=kZ3lpzVZWuRbUhACQkQkqrRrgqA2dyab5Gg7",
+	"Economics": "https://u.pcloud.link/publink/show?code=kZNlpzVZ9jQeCGcuSTLDEUrHrHT4jzzUOuik",
+	"Child Abuse Studies & Statistics": "https://u.pcloud.link/publink/show?code=kZrxpzVZQoCkap7vnlJ4yLOld1pJ051rQ2Xy"
+};
+
 
 function listFolders() {
-  
-  // List here the name of each folder manually
-  let folders = ["Physics", "Mathematics", "Information Theory", "Information Security", "Economics", "Child Abuse Studies & Statistics"];
-  //--------------------------------------------------------------------------------------------------------------------------------------------
-  folders.forEach(listElements);
-  
-  function listElements(item) {  
-    document.getElementById("buttons").innerHTML += '<li><a href="./'+item+'">'+item+'</a></li>';	
-  }
+
+	function sortObject(folders) {
+  		return Object.keys(folders).sort().reduce(function (result, key) {
+    		result[key] = folders[key];
+    		return result;
+  		}, {});
+	}	
+	
+	let sortedFolders =  sortObject(folders);
+
+  	Object.entries(sortedFolders).forEach(listElements);
+	
+	function listElements([key, value]) {
+	document.getElementById("buttons").innerHTML += '<a href="'+value+'"><li>'+key+'</li></a>';
+  	}
 }
 
