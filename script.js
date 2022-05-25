760d43077ac3c06f5f669f05eedb14c340993252
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2022-05-25 14:28:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
 function listFolders() {
+  
+  // List here the name of each folder manually
   let folders = ["Physics", "Mathematics", "Information Theory", "Information Security", "Economics", "Child Abuse Studies & Statistics"];
+  //--------------------------------------------------------------------------------------------------------------------------------------------
   folders.forEach(listElements);
-  function listElements(folderName) {  
-    document.getElementById("buttons").innerHTML += '<li onclick="openFolder('+folderName+')>'+folderName+'</li>';
+  
+  function listElements(item) {  
+    document.getElementById("buttons").innerHTML += '<li><a href="./'+item+'">'+item+'</a></li>';	
   }
 }
 
-function x(variable_name) {
-  return document.getElementById(variable_name);
-}
-
-function openFolder(folderName) {
-  document.getElementById("folders").style.display="none";
-}
